e71d3ef0716eb292394488e83c64cd207186cdf3
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-05 14:30:12 -0600

    update vehicle class to support plate_type and define register_vehicle method for facility class, update tests for facility_spec

--- lib/facility.rb
+++ lib/facility.rb
@@ -1,14 +1,36 @@
 class Facility
-  attr_reader :name, :address, :phone, :services
+  attr_reader :name, :address, :phone, :services, :collected_fees
 
   def initialize(facility)
     @name = facility[:name]
     @address = facility[:address]
     @phone = facility[:phone]
     @services = []
+    @collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+end
+
+def register_vehicle(vehicle)
+  
+  @registration_date = Date.today
+
+  if vehicle.antique? == true
+    @collected_fees + 25
+    @plate_type[:antique]
+    @registered_vehicles << vehicle
+    
+  elsif vehicle.electric_vehicle? == true
+    @collected_fees + 200
+    @plate_type[:ev]
+    @registered_vehicles << vehicle
+  
+  else vehicle
+    @collected_fees + 100
+    @plate_type[:basic]
+    @registered_vehicles << vehicle
   end
 end
