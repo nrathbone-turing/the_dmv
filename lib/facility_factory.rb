07ddbbb5c1fdd47a764f930ecdd465a3aff2ae8f
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-06 10:45:53 -0600

    Create skeleton for create_facilities method and create tests based on provided interaction pattern with new external dataset

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,3 +1,16 @@
 class FacilityFactory
-  
+  def create_facilities(external_dataset)
+    external_dataset.map do |facility_record|
+      Facility.new({
+      :name => facility_record[],
+      :address => facility_record[],
+      :phone => facility_record[],
+      :services => facility_record[],
+      :registered_vehicles => facility_record[],
+      :collected_fees= => facility_record[]
+      })
+    end
+  end
+
+
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 RSpec.describe FacilityFactory do
   
+  before(:each) do
+    @facility_factory = FacilityFactory.new
+    @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
+  end
+
+  it 'exists' do
+    expect(@facility_factory).to be_an_instance_of(FacilityFactory)
+    
+    #@co_facilities is an array of hashes, where each hash represents an individual facility record
+    #from the Colorado DMV Office Locations external data source
+    expect(@co_dmv_office_locations).to be_an(Array)
+
+    #printing return value for the first element in the @co_facilities array to make sure it works
+    p @co_dmv_office_locations[0]
+
+    #printing the return value for all of the keys within the hash of the first facility record element in the @co_facilities array to make sure the mapping logic is using the correct keys
+    p @cco_dmv_office_locations[0].keys
+  end
+  
+  it 'creates facility objects from external data source' do
+    @facilities = factory.create_facilities(@cco_dmv_office_locations)
+
+    expect(@facilities).to be_an(Array)
+    expect(@facilities[0]).to be_a(Facility)
+    
+    end
+
+  
 end
