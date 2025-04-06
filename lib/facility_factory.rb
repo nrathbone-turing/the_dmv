94044a52b6d8eee0d8e6adc5218e9c4d05f2ac6c
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-06 12:20:20 -0600

    Refactor FacilityFactory class so that the create_facilities method does not try to pass the entire external_dataset when it calls for the full_address method which I also refactored so that it correctly parses the individual items within the facility hash into a single string which my Facility class expects

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -1,23 +1,28 @@
 class FacilityFactory
   def create_facilities(external_dataset)
+    # Iterates over each facility record (note that each one is already a hash)
+    # and transforms it into a Facility object using the values from the hash
     external_dataset.map do |facility_record|
       Facility.new({
       :name => facility_record[:dmv_office],
-      :address => full_address(external_dataset),
+      :address => full_address(facility_record),
       :phone => facility_record[:phone]
       })
     end
   end
 
-  #combines the hash key values from the facility record that store individual components of an address from multiple strings
-  #into a single string that matches what the Facility class expects and can pass as a valid argument for the :address parameter
-  def full_address(external_dataset)
-    "#{:address_li} #{:address_1} #{:city} #{:state} #{:zip}"
+  # Combines individual address components from the facility hash into a single string that matches the format expected by the Facility class
+  # This parameter is named differently from the block variable above to reinforce that each method has a single, distinct responsibility (SRP),
+  # even though they both reference the same kind of data (a facility hash in this case)
+  def full_address(raw_location_data)
+    "#{raw_location_data[:address_li]} #{raw_location_data[:address__1]} #{raw_location_data[:city]} #{raw_location_data[:state]} #{raw_location_data[:zip]}"
   end
 
   #transforms the hash key values from the facility record for services offered from a single string
   #into an array of strings that matches what the Facility class expects and can pass as a valid argument for the :services parameter
   def transform_services(external_dataset)
+    
+    transform_services(@facility_record).split(' ')
     transform_services(@facility_record[:services_p]).each do |service|
       facility.add_service(service)
     end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -20,7 +20,7 @@ RSpec.describe FacilityFactory do
     # :address_li=>"2855 Tremont Place", :address__1=>"Suite 118", :city=>"Denver", :state=>"CO", :zip=>"80205", :phone=>"(720) 865-4600",
     # :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals; VIN inspections",
     # :parking_no=>"parking available in the lot at the back of the bldg (Glenarm Street)", :photo=>"images/Tremont.jpg", :address_id=>"175164", :":@computed_region_nku6_53ud"=>"1444"}
-
+    #binding.pry
     #printing the return value for all of the keys within the hash of the first facility record element in the @co_dmv_office_locations array to make sure the mapping logic is using the correct keys
     p @co_dmv_office_locations[0].keys
     #=> [:the_geom, :dmv_id, :dmv_office, :address_li, :address__1, :city, :state, :zip, :phone, :hours, :services_p, :parking_no, :photo, :address_id, :":@computed_region_nku6_53ud"]
