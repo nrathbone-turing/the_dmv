18ef766fcb2da764cb56c34db5236d1eeb3a9421
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-06 11:12:50 -0600

    Update tests to return the first element in the @co_dmv_office_locations array of hashes from the external database along with the keys used to make sure my method uses the correct keys

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -2,13 +2,24 @@ class FacilityFactory
   def create_facilities(external_dataset)
     external_dataset.map do |facility_record|
       Facility.new({
-      :name => facility_record[],
-      :address => facility_record[],
-      :phone => facility_record[],
-      :services => facility_record[],
-      :registered_vehicles => facility_record[],
-      :collected_fees= => facility_record[]
+      :name => facility_record[:dmv_office],
+      :address => full_address(external_dataset),
+      :phone => facility_record[:phone]
       })
+    end
+  end
+
+  #combines the hash key values from the facility record that store individual components of an address from multiple strings
+  #into a single string that matches what the Facility class expects and can pass as a valid argument for the :address parameter
+  def full_address(external_dataset)
+    "#{:address_li} #{:address_1} #{:city} #{:state} #{:zip}"
+  end
+
+  #transforms the hash key values from the facility record for services offered from a single string
+  #into an array of strings that matches what the Facility class expects and can pass as a valid argument for the :services parameter
+  def transform_services(external_dataset)
+    transform_services(@facility_record[:services_p]).each do |service|
+      facility.add_service(service)
     end
   end
 
