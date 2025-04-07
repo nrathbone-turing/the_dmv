33c7d9e31c2487522b4e08a8536dc8ed4884c2a1
Nick Rathbone <nrathbone@joinhandshake.com>
2025-04-06 21:24:05 -0600

    Refactor the facility_factory test to update comments for pry usage and rename + add the phone and address attributes to my transformation check test blocks

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -22,19 +22,19 @@ RSpec.describe FacilityFactory do
     expect(@co_dmv_office_locations[0][:state]).to eq("CO")
     expect(@co_dmv_office_locations[0][:zip]).to eq("80205")
 
-    #printing return value for the first element in the @co_dmv_office_locations array to make sure it works
-    #p @co_dmv_office_locations[0]
+    #testing return value for the first element in the @co_dmv_office_locations array to make sure it works
+    #pry(main)> @co_dmv_office_locations[0]
     #=> {:the_geom=>{:type=>"Point", :coordinates=>[-104.97443112500002, 39.75525297420336]}, :dmv_id=>"1", :dmv_office=>"DMV Tremont Branch",
     # :address_li=>"2855 Tremont Place", :address__1=>"Suite 118", :city=>"Denver", :state=>"CO", :zip=>"80205", :phone=>"(720) 865-4600",
     # :hours=>"Mon, Tue, Thur, Fri  8:00 a.m.- 4:30 p.m. / Wed 8:30 a.m.-4:30 p.m.", :services_p=>"vehicle titles, registration, renewals; VIN inspections",
     # :parking_no=>"parking available in the lot at the back of the bldg (Glenarm Street)", :photo=>"images/Tremont.jpg", :address_id=>"175164", :":@computed_region_nku6_53ud"=>"1444"}
     
-    #printing the return value for all of the keys within the hash of the first facility record element in the @co_dmv_office_locations array to make sure the mapping logic is using the correct keys
-    #p @co_dmv_office_locations[0].keys
+    #testing the return value for all of the keys within the hash of the first facility record element in the @co_dmv_office_locations array to make sure the mapping logic is using the correct keys
+    #pry(main)> @co_dmv_office_locations[0].keys
     #=> [:the_geom, :dmv_id, :dmv_office, :address_li, :address__1, :city, :state, :zip, :phone, :hours, :services_p, :parking_no, :photo, :address_id, :":@computed_region_nku6_53ud"]
 
-    #printing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
-    #p @co_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
+    #testing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
+    #pry(main)> @co_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
     #=> [:the_geom, :dmv_id, :dmv_office, :address_li, :address__1, :city, :state, :zip, :phone, :hours, :services_p, :parking_no, :photo, :address_id, :":@computed_region_nku6_53ud", :location]
   end
   
@@ -42,6 +42,7 @@ RSpec.describe FacilityFactory do
     @co_dmv_office_locations = DmvDataService.new.co_dmv_office_locations
     @facilities = @facility_factory.create_facilities(@co_dmv_office_locations)
 
+    #testing the first element (facility hash)
     raw_location_data = @co_dmv_office_locations[0]
 
     expect(raw_location_data[:address_li]).to eq("2855 Tremont Place")
@@ -49,6 +50,11 @@ RSpec.describe FacilityFactory do
     expect(raw_location_data[:city]).to eq("Denver")
     expect(raw_location_data[:state]).to eq("CO")
     expect(raw_location_data[:zip]).to eq("80205")
+
+    #testing the last element (facility hash)
+
+
+    #testing a random middle index element (facility hash)
     
   end
 
@@ -73,7 +79,7 @@ RSpec.describe FacilityFactory do
     expect(@facilities[0].phone).to eq(@co_dmv_office_locations[0][:phone])
     expect(@facilities[0].address).to eq("2855 Tremont Place Suite 118 Denver CO 80205")
     
-    #p @facilities[0]
+    #pry(main)> @facilities[0]
     #=> #<Facility:0x0000000105e924a0 @name="DMV Tremont Branch", @address="2855 Tremont Place Suite 118 Denver CO 80205", @phone="(720) 865-4600", @services=[], @registered_vehicles=[], @collected_fees=0>
   end
 
@@ -87,16 +93,19 @@ RSpec.describe FacilityFactory do
     it 'creates CO facility objects correctly' do
       expect(@colorado_facilities).to be_an(Array)
       expect(@colorado_facilities[0]).to be_a(Facility)
+
     end
 
-    it 'transforms address data correctly' do
+    it 'correctly transforms the first facility_record object' do
       raw_location_data = @co_dmv_office_locations[0]
       
       full_address = "#{raw_location_data[:address_li]} #{raw_location_data[:address__1]} #{raw_location_data[:city]} #{raw_location_data[:state]} #{raw_location_data[:zip]}"
       
       expect(@colorado_facilities[0].address).to eq(full_address)
-
-      #p @colorado_facilities[0]
+      expect(@colorado_facilities[0].name).to eq("DMV Tremont Branch")
+      expect(@colorado_facilities[0].phone).to eq("(720) 865-4600")
+      
+      #pry(main)> @colorado_facilities[0]
       #=> #<Facility:0x0000000105e924a0 @name="DMV Tremont Branch", @address="2855 Tremont Place Suite 118 Denver CO 80205", @phone="(720) 865-4600", @services=[], @registered_vehicles=[], @collected_fees=0>
     end
   end
@@ -106,8 +115,8 @@ RSpec.describe FacilityFactory do
     before(:each) do
       @ny_dmv_office_locations = DmvDataService.new.ny_dmv_office_locations
       
-      #printing return value for the first element in the @ny_dmv_office_locations array to make sure it works
-      #p @ny_dmv_office_locations[0]
+      #testing return value for the first element in the @ny_dmv_office_locations array to make sure it works
+      #pry(main)> @ny_dmv_office_locations[0]
       #=> {:office_name=>"LAKE PLACID", :office_type=>"COUNTY OFFICE", :street_address_line_1=>"2693 MAIN STREET", :city=>"LAKE PLACID", :state=>"NY", :zip_code=>"12946",
       # :monday_beginning_hours=>"CLOSED", :monday_ending_hours=>"CLOSED", :georeference=>{:type=>"Point", :coordinates=>[-73.98278, 44.28213]}, :":@computed_region_yamh_8v7k"=>"430", 
       # :":@computed_region_wbg7_3whc"=>"275", :":@computed_region_kjdx_g34t"=>"2084"}
@@ -119,28 +128,30 @@ RSpec.describe FacilityFactory do
       expect(@new_york_facilities).to be_an(Array)
       expect(@new_york_facilities[0]).to be_a(Facility)
   
-      #printing the return value for all of the keys within the hash of the first facility record element in the @ny_dmv_office_locations array to make sure the mapping logic is using the correct keys
-      #p @ny_dmv_office_locations[0].keys
+      #testing the return value for all of the keys within the hash of the first facility record element in the @ny_dmv_office_locations array to make sure the mapping logic is using the correct keys
+      #pry(main)> @ny_dmv_office_locations[0].keys
       #=> [:office_name, :office_type, :street_address_line_1, :city, :state, :zip_code, :monday_beginning_hours, :monday_ending_hours,
       # :georeference, :":@computed_region_yamh_8v7k", :":@computed_region_wbg7_3whc", :":@computed_region_kjdx_g34t"]
 
-      #printing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
-      #p @ny_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
+      #testing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
+      #pry(main)> @ny_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
       # => [:office_name, :office_type, :street_address_line_1, :city, :state, :zip_code, :monday_beginning_hours, :monday_ending_hours,
       # :georeference, :":@computed_region_yamh_8v7k", :":@computed_region_wbg7_3whc", :":@computed_region_kjdx_g34t", :public_phone_number,
       # :tuesday_beginning_hours, :tuesday_ending_hours, :wednesday_beginning_hours, :wednesday_ending_hours, :thursday_beginning_hours, :thursday_ending_hours,
       # :friday_beginning_hours, :friday_ending_hours, :street_address_line_2, :public_phone_extension, :saturday_beginning_hours, :saturday_ending_hours]
     end
 
-    it 'transforms address data correctly' do
+    it 'correctly transforms the first facility_record object' do
       raw_location_data = @ny_dmv_office_locations[0]
      
       full_address = "#{raw_location_data[:street_address_line_1]} #{raw_location_data[:city]} #{raw_location_data[:state]} #{raw_location_data[:zip_code]}"
       
       expect(@new_york_facilities[0].address).to eq(full_address)
-      
-      #p @new_york_facilities[0]
-      #=> <Facility:0x0000000103c8f970 @name="LAKE PLACID", @address="2693 MAIN STREET  LAKE PLACID NY 12946", @phone=nil, @services=[], @registered_vehicles=[], @collected_fees=0>
+      expect(@new_york_facilities[0].name).to eq("LAKE PLACID")
+      expect(@new_york_facilities[0].phone).to eq("(417) 548-7332")
+
+      #pry(main)> @new_york_facilities[0]
+      #=> <Facility:0x0000000103c8f970 @name="LAKE PLACID", @address="2693 MAIN STREET  LAKE PLACID NY 12946", @phone="(417) 548-7332", @services=[], @registered_vehicles=[], @collected_fees=0>
     end
   end
 
@@ -149,8 +160,8 @@ RSpec.describe FacilityFactory do
     before(:each) do
       @mo_dmv_office_locations = DmvDataService.new.mo_dmv_office_locations
       
-      #printing return value for the first element in the @mo_dmv_office_locations array to make sure it works
-      #p @mo_dmv_office_locations[0]
+      #testing return value for the first element in the @mo_dmv_office_locations array to make sure it works
+      #pry(main)> @mo_dmv_office_locations[0]
       #=> {:number=>"032", :type=>"1MV", :name=>"Sarcoxie", :address1=>"111 N 6th", :city=>"Sarcoxie", :state=>"MO", :zipcode=>"64862", :phone=>"(417) 548-7332", :fax=>"(417) 548-3108",
       # :size=>"2", :email=>"sarcoxie.licenseoffice@lo.mo.gov", :agent=>"City of Sarcoxie", :officemanager=>"Heather Swan", :contractmanager=>"Don Triplett",
       # :daysopen=>"Monday & Friday 8:30-5:00, Tuesday - Thursday 8:30-4:30", :daysclosed=>"Monday & Friday 1:00-1:30",
@@ -165,25 +176,27 @@ RSpec.describe FacilityFactory do
       expect(@missouri_facilities).to be_an(Array)
       expect(@missouri_facilities[0]).to be_a(Facility)
 
-      #printing the return value for all of the keys within the hash of the first facility record element in the @mo_dmv_office_locations array to make sure the mapping logic is using the correct keys
-      #p @mo_dmv_office_locations[0].keys
+      #testing the return value for all of the keys within the hash of the first facility record element in the @mo_dmv_office_locations array to make sure the mapping logic is using the correct keys
+      #pry(main)> @mo_dmv_office_locations[0].keys
       #=> [:number, :type, :name, :address1, :city, :state, :zipcode, :phone, :fax, :size, :email, :agent, :officemanager, :contractmanager,
       # :daysopen, :daysclosed, :holidaysclosed, :additionaldaysclosed, :latlng, :":@computed_region_ny2h_ckbz", :":@computed_region_c8ar_jsdj", :":@computed_region_ikxf_gfzr"]
 
-      #printing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
-      #p @mo_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
+      #testing the return value for all of the keys used by any hash within the array of facility hashes to make sure I am accounting for multiple address lines or other differences that may exist between first element and others
+      #pry(main)> @mo_dmv_office_locations.flat_map { |facility_record| facility_record.keys }.uniq
       #=> [:number, :type, :name, :address1, :city, :state, :zipcode, :phone, :fax, :size, :email, :agent, :officemanager, :contractmanager, :daysopen, :daysclosed, :holidaysclosed, :additionaldaysclosed,
       # :latlng, :":@computed_region_ny2h_ckbz", :":@computed_region_c8ar_jsdj", :":@computed_region_ikxf_gfzr", :facebook_url, :managercontactnumber, :othercontactinfo, :dorregionnumber, :remarks, :additional_license_office_info]
     end
 
-    it 'transforms address data correctly' do
+    it 'correctly transforms the first facility_record object' do
       raw_location_data = @mo_dmv_office_locations[0]
       
       full_address = "#{raw_location_data[:address1]} #{raw_location_data[:city]} #{raw_location_data[:state]} #{raw_location_data[:zipcode]}"
       
       expect(@missouri_facilities[0].address).to eq(full_address)
-
-      #p @missouri_facilities[0]
+      expect(@missouri_facilities[0].name).to eq("Sarcoxie")
+      expect(@missouri_facilities[0].phone).to eq("(417) 548-7332")
+      
+      #pry(main)> @missouri_facilities[0]
       #=> #<Facility:0x00000001042a8bb8 @name="Sarcoxie", @address="111 N 6th  Sarcoxie MO 64862", @phone="(417) 548-7332", @services=[], @registered_vehicles=[], @collected_fees=0>
 
     end
